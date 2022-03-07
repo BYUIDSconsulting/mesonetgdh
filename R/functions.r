06060af0c30a02f5c685ebe1bd5729dd54e74eac
marsbizi <marsbizi@gmail.com>
2022-03-07 16:08:16 -0700

    mars example

---
+++ R/functions.r
@@ -0,0 +1,17 @@
+#' @title Get Elevations
+#' @param df is the dataframe as output from the 'all_fields' function
+#' @example df = makeaneexample
+#' get_elev(df)
+#' @export
+get_elev <-  function(df){
+  
+  elevation <- c()
+  
+  for (i in 1:nrow(df)) {
+    coords <- data.frame(x=df$lon[i], y=df$lat[i])
+    elev <- get_elev_point(coords, unit = 'feet', src='epqs', prj="EPSG:4326")
+    elevation <- c(elevation, elev[[1]])
+  }
+  
+  return(elevation)
+}

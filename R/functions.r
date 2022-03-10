a4d8d51ea69f1fe1d01ec5e6cbc02f0effd02260
marsbizi <marsbizi@gmail.com>
2022-03-09 18:13:04 -0700

    update get_elev

--- R/functions.r
+++ R/functions.r
@@ -3,15 +3,15 @@
 #' @example df = makeaneexample
 #' get_elev(df)
 #' @export
+
 get_elev <-  function(df){
   
-  elevation <- c()
-  
-  for (i in 1:nrow(df)) {
-    coords <- data.frame(x=df$lon[i], y=df$lat[i])
-    elev <- elevatr::get_elev_point(coords, unit = 'feet', src='epqs', prj="EPSG:4326")
-    elevation <- c(elevation, elev[[1]])
-  }
+  # define coordinate columns
+  coords <- data.frame(x=df$lon, y=df$lat)
+  # get elevation from source
+  elev <- elevatr::get_elev_point(coords, unit = 'feet', src='epqs', prj="EPSG:4326")
+  # extract elevation column
+  elevation <- elev[[1]]
   
   return(elevation)
 }

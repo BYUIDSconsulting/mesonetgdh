fcce418b328a3c051f25ff2a5cbcbad1d8dbb85e
ellieanderson <and18043@byui.edu>
2022-03-28 15:31:28 -0600

    changed temp col name

--- R/functions.r
+++ R/functions.r
@@ -171,7 +171,7 @@ elev_change <- function(data){
 }
 
 #' @title Summary of temperature aggregations
-#' @param data is the dataframe that contains the temperatures from each station. Temp column should be named "air_temp_f"
+#' @param data is the dataframe that contains the temperatures from each station. Temp column should be named "air_temp_set_1"
 #' @example data 
 #' p <- elev_change(data)
 #' @export
@@ -180,8 +180,8 @@ create_field_weather_data <- function(data){
   # dat <- field_station %>% left_join(station_temp_data)
   data %>% 
     group_by(field_id) %>%
-    mutate(temp_min = min(air_temp_f),
-           temp_max = max(air_temp_f),
+    mutate(temp_min = min(air_temp_set_1),
+           temp_max = max(air_temp_set_1),
            temp_avg = mean(air_temp_set_1)) %>%
     #temp_wt_avg = weighted(temp)) %>%
     select(field_id, Date, Hour, temp_min, temp_max, temp_avg) %>%

461280065893c9e633bb3de757110dc798b8af9b
ellieanderson <and18043@byui.edu>
2022-03-28 15:28:17 -0600

    Added create_field_weather_data

--- R/functions.r
+++ R/functions.r
@@ -169,3 +169,21 @@ elev_change <- function(data){
     summarize(elev_dif = abs(field_elev - station_elev)) %>%
     unique()
 }
+
+#' @title Summary of temperature aggregations
+#' @param data is the dataframe that contains the temperatures from each station. Temp column should be named "air_temp_f"
+#' @example data 
+#' p <- elev_change(data)
+#' @export
+
+create_field_weather_data <- function(data){
+  # dat <- field_station %>% left_join(station_temp_data)
+  data %>% 
+    group_by(field_id) %>%
+    mutate(temp_min = min(air_temp_f),
+           temp_max = max(air_temp_f),
+           temp_avg = mean(air_temp_f)) %>%
+    #temp_wt_avg = weighted(temp)) %>%
+    select(field_id, Date, Hour, temp_min, temp_max, temp_avg) %>%
+    distinct()
+}

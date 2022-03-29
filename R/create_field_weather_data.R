5c073803415c66219194003e68a48113003b6582
marsbizi <marsbizi@gmail.com>
2022-03-29 13:00:08 -0600

    update var names

--- R/create_field_weather_data.R
+++ R/create_field_weather_data.R
@@ -7,11 +7,11 @@
 create_field_weather_data <- function(data){
   # dat <- field_station %>% left_join(station_temp_data)
   data %>% 
-    group_by(field_id) %>%
-    mutate(temp_min = min(air_temp_set_1),
-           temp_max = max(air_temp_set_1),
-           temp_avg = mean(air_temp_set_1)) %>%
+    group_by(FIELD_ID, Date, Hour) %>%
+    mutate(temp_min = min(temp_avg),
+           temp_max = max(temp_avg),
+           temp_avg = mean(temp_avg)) %>%
     #temp_wt_avg = weighted(temp)) %>%
-    select(field_id, Date, Hour, temp_min, temp_max, temp_avg) %>%
+    select(FIELD_ID, Date, Hour, temp_min, temp_max, temp_avg) %>%
     distinct()
 }

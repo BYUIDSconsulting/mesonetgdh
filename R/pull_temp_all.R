60b7bede7103c6f80a82a38bc5fe836e2139ec48
Logan Herrmeyer <logern5@gmail.com>
2022-05-02 21:45:47 -0600

    Add initial fast pull temp function, needs fixing

---
+++ R/pull_temp_all.R
@@ -0,0 +1,34 @@
+#' @title Request the temperature data from MesoWest, but fast
+#' @param df is the fields_stations dataset derived from the find_closest_station() function. 
+#' @export
+global_resp <- NULL
+pull_temp_all <- function(df) {
+  
+  stations_date <- df %>% 
+    select(station_id, harvest_date, seeding_date)
+
+  base_url <- 'https://api.synopticdata.com/v2'
+
+  df_request <- NULL
+  
+  start_date <- paste0(gsub('-', '', min(stations_date$seeding_date)), '0000')
+  end_date <- paste0(gsub('-', '', min(stations_date$harvest_date)), '0000')
+  stations_str <- stringr::str_c((df$station_id %>% unique()),collapse=",")
+  req <- paste0('/stations/timeseries?stid=', stations_str
+                ,'&vars=air_temp&fields=stid,longitude,latitude,elevation'
+                ,'&units=english,temp|F'
+                ,'&start=', start_date, '&end=', end_date)
+  req_url <- paste0(base_url, req, '&token=', token)
+  response <- jsonlite::fromJSON(req_url, simplifyMatrix = TRUE)
+  
+  id <- response$STATION$STID
+  
+  resp <- data.frame(lapply(response$STATION$OBSERVATIONS, unlist)) 
+  print(tibble(resp) %>% head(5))
+  df_request <- rbind(df_request, resp) 
+  df_request <- df_request %>% 
+    mutate(year=lubridate::year(date_time))
+  
+  return(df_request)
+  
+}

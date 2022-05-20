2dde9eb9c2497e0edcf4e3766fd88ddf9d9d0cc0
Logan Herrmeyer <logern5@gmail.com>
2022-05-19 20:23:19 -0600

    Add GDD info to data

--- R/download_crops.R
+++ R/download_crops.R
@@ -1,14 +1,21 @@
 library(pacman)
-pacman::p_load(DBI, odbc, tidyverse)
+pacman::p_load(DBI, odbc, tidyverse, gtools)
 
-#' @title Request crop data from Snowflake within a certain season range
+#' @title Request crop data (seed_harvest_wide) from Snowflake within a certain season range
 #' @param conn The connection to the Snowflake database
 #' @param min_year Minimum crop season year to pull from. Defaults to NA (pull all years)
 #' @param max_year Maximum crop season year to pull from. Defaults to NA (pull all years)
-#' @examples p <- pull_temp_all(df)
+#' @examples seed_harvest_wide <- download_crops(myconn, min_year=2020, max_year=2022)
 #' @returns The seed_harvest_df data frame.
 #' @export
-download_crops <- function(conn, min_year=NA, max_year=NA) {
+download_crops <- function(conn, gdd_info_formatted, min_year=NA, max_year=NA) {
+  usethis::use_data()
+  if(gtools::invalid(conn)){
+    stop("Invalid connection provided")
+  }
+  if(gtools::invalid(gdd_info_formatted)){
+    stop("Invalid gdd_info_formatted provided")
+  }
   DBI::dbGetQuery(conn, "USE FARM_PROD.FARM__PRODUCTION;")
   if(is.na(min_year)){
     min_year = 0
@@ -40,5 +47,16 @@ download_crops <- function(conn, min_year=NA, max_year=NA) {
     filter(n==1,
            !is.na(CROP_NAME)) %>% # remove unidentified crop name
     select(-n)
+  # Restructure dataset
+  seed_harvest_wide <- seed_harvest_df  %>% 
+    pivot_wider(names_from = FIELD_OPERATION_TYPE,
+                values_from = DATE) %>% 
+    drop_na() %>% 
+    mutate(harvest = as.Date(harvest),
+           seeding = as.Date(seeding)) %>% 
+    rename(harvest_date = harvest,
+           seeding_date = seeding) %>% 
+    filter(CROP_NAME %in% gdd_info_formatted$CROP, # FROM AVAILABLE CROP THRESHOLD DATA
+           CROP_SEASON > min_year) # This originally said 2019
   return(seed_harvest_df)
 }
---
+++ R/download_fields.R
@@ -0,0 +1,37 @@
+library(pacman)
+pacman::p_load(DBI, odbc, tidyverse)
+
+#' @title Request fields data (all_fields1) from Snowflake
+#' @param conn The connection to the Snowflake database
+#' @examples all_fields1 <- download_crops(myconn) %>% filter(state %in% c('AZ','ID','UT'))
+#' @returns The seed_harvest_df data frame.
+#' @export
+download_crops <- function(conn, min_year=NA, max_year=NA) {
+  fields_query <- "
+  SELECT fi.ID as FIELD_ID,
+      CAST(
+        PARSE_JSON(b.MULTIPOLYGONS)['multipolygons'][0]['rings'][0]['points'][0]['lat']
+        AS DECIMAL(12,8))
+        AS LAT,
+      CAST(PARSE_JSON(b.MULTIPOLYGONS)['multipolygons'][0]['rings'][0]['points'][0]['lon']
+        AS DECIMAL(12,8))
+        AS LON
+  FROM FIELDS fi
+  INNER JOIN BOUNDARIES b
+      ON fi.ID = b.FIELD_ID
+  ORDER BY FIELD_ID;
+  "
+  # Get all fields coordinates from the database
+  all_fields <- DBI::dbGetQuery(myconn, fields_query) %>%
+    rename(lat = LAT, lon = LON)
+  all_fields1 <- all_fields %>%
+    # Get state names and elevations
+    mutate(state = mesonetgdh::get_state(.),
+         field_elev = mesonetgdh::get_elev(.)) %>% 
+    drop_na() %>% 
+    # Using only fields in Arizona, Idaho, Utah
+    filter(state %in% c('AZ', 'ID', 'UT')) %>% 
+    rename(field_lat=lat,
+           field_lon=lon)
+  return(all_fields1)
+}

f446efb73c96126ebac18b3f4aeacab5aadf0020
Logan Herrmeyer <logern5@gmail.com>
2022-06-09 20:35:23 -0600

    Add database functions

---
+++ R/db_functions.r
@@ -0,0 +1,116 @@
+library(pacman)
+pacman::p_load(dplyr, DBI)
+
+#' @title Get all fields from the Snowflake database
+#' @param myconn The connection to the Snowflake database
+#' @examples
+#' all_fields1 <- get_all_fields(myconn)
+#' @export
+get_all_fields <- function(myconn){
+  fields_query <- "
+  SELECT fi.ID as FIELD_ID,
+      CAST(
+        PARSE_JSON(b.MULTIPOLYGONS)['multipolygons'][0]['rings'][0]['points'][0]['lat']
+        AS DECIMAL(12,8))
+        AS LAT,
+      CAST(PARSE_JSON(b.MULTIPOLYGONS)['multipolygons'][0]['rings'][0]['points'][0]['lon']
+        AS DECIMAL(12,8))
+        AS LON
+  FROM FIELDS fi
+  INNER JOIN BOUNDARIES b
+      ON fi.ID = b.FIELD_ID
+  ORDER BY FIELD_ID;
+  "
+  # Get all fields coordinates from the database
+  all_fields <- DBI::dbGetQuery(myconn, fields_query) %>%
+    rename(lat = LAT, lon = LON) %>%
+    # Let's make sure we don't repeat fields
+    distinct(FIELD_ID, .keep_all=TRUE)
+  all_fields1 <- all_fields %>% 
+    # We don't want to repeat fields
+    # add the states and elevation using the weather package
+    mutate(state = mesonetgdh::get_state(.)) %>% 
+    drop_na() %>% 
+    # Using only fields in Arizona, Idaho, Utah
+    filter(state %in% c('AZ', 'ID', 'UT')) %>% 
+    # Get elevation after we have the states
+    mutate(field_elev = mesonetgdh::get_elev(.)) %>%
+    drop_na() %>%
+    rename(field_lat=lat,
+           field_lon=lon)
+  return(all_fields1)
+}
+
+
+#' @title Get latest date with GDHs from the Snowflake database
+#' @param myconn The connection to the Snowflake database
+#' @examples
+#' latest_date <- get_latest_date(myconn)
+#' @export
+get_latest_date <- function(myconn){
+  # Latest GDH date in the database, get temperatures for everything after this
+  latest_date <- DBI::dbGetQuery(
+    myconn,
+    "SELECT * FROM GDH_BYUI_DEV ORDER BY GDH_DATE DESC LIMIT 1;"
+    ) %>%
+    pull(GDH_DATE)
+  return(GDH_DATE)
+}
+
+#' @title Get the wide seed harvest dataframe (based on field crops data) from the Snowflake database
+#' @param myconn The connection to the Snowflake database
+#' @param gdd_info_formatted Growing Degree Day temperature threshold data
+#' @examples
+#' seed_harvest_df <- get_seed_harvest(myconn)
+#' @export
+get_seed_harvest_wide <- function(myconn, gdd_info_formatted){
+  # crop information, seeding and harvest dates from the database
+  crops_query <- "
+  SELECT FIELD_ID, FIELD_OPERATION_TYPE, CROP_NAME,
+    CAST(CROP_SEASON AS INT) AS CROP_SEASON, START_DATE, END_DATE
+    FROM FIELD_OPERATIONS
+    ORDER BY FIELD_ID; 
+  "
+  field_crops <- DBI::dbGetQuery(myconn, crops_query)
+  seed_harvest_df <- field_crops %>%
+    # Using the START_DATE column for dates
+    rename(DATE = START_DATE) %>% 
+    select(-END_DATE) %>% 
+    # remove duplicates
+    distinct() %>% 
+    # select fields with one seeding or harvest entry for a unique crop and season 
+    group_by(FIELD_ID, CROP_SEASON, CROP_NAME, FIELD_OPERATION_TYPE) %>% 
+    mutate(n=n()) %>% 
+    ungroup() %>% 
+    filter(n==1,
+           !is.na(CROP_NAME)) %>% # remove unidentified crop name
+    select(-n)
+  # Restructure dataset
+  seed_harvest_wide <- seed_harvest_df  %>% 
+    pivot_wider(names_from = FIELD_OPERATION_TYPE,
+                values_from = DATE) %>% 
+    drop_na() %>% 
+    mutate(harvest = as.Date(harvest),
+           seeding = as.Date(seeding)) %>% 
+    rename(harvest_date = harvest,
+           seeding_date = seeding) %>% 
+    filter(CROP_NAME %in% gdd_info_formatted$CROP, # FROM AVAILABLE CROP THRESHOLD DATA
+           CROP_SEASON > 2019)
+  return(seed_harvest_wide)
+}
+
+#' @title Remove duplicate rows from a Snowflake table 
+#' @param myconn A connection to the Snowflake database
+#' @param table The table to remove duplicates from
+#' @examples
+#' remove_duplicates(table)
+#' @export
+remove_duplicates <- function(myconn, table){
+  cmds <- c(
+    sprintf("CREATE TABLE new_table LIKE %s COPY GRANTS;",table),
+    sprintf("INSERT INTO new_table SELECT DISTINCT * FROM %s;",table),
+    sprintf("ALTER TABLE %s SWAP WITH new_table;",table),
+    "DROP TABLE new_table;"
+  )
+  lapply(cmds, function(cmd){DBI::dbGetQuery(myconn, cmd)})
+}
--- R/pull_temp_all.R
+++ R/pull_temp_all.R
@@ -1,20 +1,20 @@
-#' @title Request the temperature data from MesoWest, but fast
-#' @param df is the fields_stations dataset derived from the find_closest_station() function. 
-#' @examples p <- pull_temp_all(df)
-#' @export
-pull_temp_all <- function(df) {
-  
-  stations_date <- df %>% 
-    select(station_id, harvest_date, seeding_date)
+make_ranges_day <- function(from, to = Sys.Date(), n = 5){
+  from <- as.Date(from)
+  to <- if(to == Sys.Date()) Sys.Date() - 1 else as.Date(to)
+  by = paste(n, "days")
+  starts <- seq(from, to - n + 1, by = by)
+  ends <- seq(from + n - 1, to, by = by)
+  last <- length(starts)
+  remaining <- to - ends[last] + 1
+  if(remaining != 0){
+    starts <- c(starts, ends[last] + 1)
+    ends <- c(ends, to)
+  }
+  return(data.frame(starts, ends))
+}
 
+pull_temp_block <- function(df, start_date, end_date, uniq_stations){
   base_url <- 'https://api.synopticdata.com/v2'
-
-  df_request <- NULL
-  
-  # Download data for all stations
-  start_date <- paste0(gsub('-', '', min(stations_date$seeding_date)), '0000')
-  end_date <- paste0(gsub('-', '', min(stations_date$harvest_date)), '0000')
-  uniq_stations <- df$station_id %>% unique()
   stations_str <- stringr::str_c(uniq_stations,collapse=",")
   req <- paste0('/stations/timeseries?stid=', stations_str
                 ,'&vars=air_temp&fields=stid,longitude,latitude,elevation'
@@ -22,7 +22,6 @@ pull_temp_all <- function(df) {
                 ,'&start=', start_date, '&end=', end_date)
   req_url <- paste0(base_url, req, '&token=', token)
   response <- jsonlite::fromJSON(req_url, simplifyMatrix = TRUE)
-  
   # Loop through each station and add its info to the data frame
   for(i in 1:length(uniq_stations)){
     id <- uniq_stations[i]
@@ -30,9 +29,45 @@ pull_temp_all <- function(df) {
       mutate(station_id = id)
     df_request <- rbind(df_request, resp)
   }
-
   df_request <- df_request %>% 
     mutate(year=lubridate::year(date_time))  
+  return(df_request)
+}
+
+#' @title Request the temperature data from MesoWest, but fast
+#' @param df is the fields_stations dataset derived from the find_closest_station() function. 
+#' @examples p <- pull_temp_all(df)
+#' @export
+pull_temp_all <- function(df, min_date=NA, max_date=NA) {
+  
+  stations_date <- df %>% 
+    select(station_id, harvest_date, seeding_date)
+
+  df_request <- as.data.frame(NULL)
+  
+  # Get begining and end dates
+  start_date <- paste0(gsub('-', '', min(stations_date$seeding_date)), '0000')
+  if(!is.na(min_date)){
+    start_date <- paste0(gsub('-', '', min_date), '0000')
+  }
+  end_date <- paste0(gsub('-', '', max(stations_date$harvest_date)), '0000')
+  if(!is.na(max_date)){
+    end_date <- paste0(gsub('-', '', max_date), '0000')
+  }
+  print(start_date)
+  print(end_date)
+  date_range_df <- make_ranges_day(min_date, max_date, n=30)
+  
+  # Get data for each date block (the api only allows a certain number of days)
+  uniq_stations <- df$station_id %>% unique()
+  for(i in 1:nrow(date_range_df)){
+    row <- date_range_df[i,]
+    df_request <- rbind(
+      df_request,
+      pull_temp_block(df, row$starts, row$ends, uniq_stations)
+    )
+  }
+  
   return(df_request)
   
 }
